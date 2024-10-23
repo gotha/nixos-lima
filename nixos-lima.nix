87678e5a7847751ffb00c5ae2b63a3b830a2ce60
Alain Lehmann <alain.lehmann@gmail.com>
2024-10-23 23:42:22 +0200

    accept flake url not just vm name

--- nixos-lima.nix
+++ nixos-lima.nix
@@ -15,19 +15,26 @@
     runtimeInputs = [lima-bin nixos-anywhere nixos-rebuild diffutils jq nix gnused];
     text = ''
       function load_configuration() {
-          CONFIG=".#nixosConfigurations.$NAME.config"
+          CONFIG="$FLAKE#nixosConfigurations.$NAME.config"
           CONFIG_JSON="$(nix eval --json "$CONFIG.lima")"
           USER_NAME="$(echo "$CONFIG_JSON" | jq -r .user.name)"
           SSH_PORT="$(echo "$CONFIG_JSON" | jq -r .ssh.localPort)"
           THE_TARGET="$USER_NAME@localhost"
       }
 
-      NAME=''${1:-}
+      FLAKE_NAME=''${1:-}
       CMD=''${2:-}
       shift 2 || (
         sed -E -n -e 's/^[[:space:]]+([^[:space:]]+)\)/usage: nixos-lima <VM_NAME> \1/p' "$0"
+        echo "   VM_NAME is a flake attribute for a nixosConfiguration.VM_NAME"
+        echo "           defaults to flake in current working dir if no no flake path given"
         exit 1
       )
+
+      # extract FLAKE and NAME (and default to flake in current folder)
+      if [[ ! "$FLAKE_NAME" =~ "#" ]]; then FLAKE_NAME=".#$FLAKE_NAME"; fi
+      NAME=''${FLAKE_NAME#*#}
+      FLAKE=''${FLAKE_NAME%#*}
 
       case "$CMD" in
         reboot)
@@ -54,9 +61,7 @@
           echo "# Welcome to the nixos-lima utility #"
           echo "#####################################"
 
-          FLAKE=".#$NAME"
-          echo "# NIXOS-LIMA: starting VM $FLAKE -- creating and updating if necessary"
-
+          echo "# NIXOS-LIMA: starting VM $NAME -- creating and updating if necessary"
           echo "# NIXOS-LIMA: extract configuration parameters from nix module"
           load_configuration
 
@@ -69,7 +74,10 @@
             limactl start "$NAME"
 
             echo "# NIXOS-LIMA: install nixos with nixos-anywhere"
-            nixos-anywhere --flake "$FLAKE" --build-on-remote "$THE_TARGET" -p "$SSH_PORT" --post-kexec-ssh-port "$SSH_PORT"
+            nixos-anywhere \
+              --build-on-remote "$THE_TARGET" -p "$SSH_PORT" \
+              --post-kexec-ssh-port "$SSH_PORT" \
+              --flake "$FLAKE_NAME"
 
             echo "# NIXOS-LIMA: ssh-keyscan to check if vm is up-and-running"
             while ! ssh-keyscan -4 -p "$SSH_PORT" localhost; do sleep 2; done
@@ -90,10 +98,10 @@
           fi
           echo "NIXOS-LIMA: vm is running"
 
-          echo "# NIXOS-LIMA: Deploying $FLAKE to $THE_TARGET"
+          echo "# NIXOS-LIMA: Deploying $FLAKE_NAME to $THE_TARGET"
           export NIX_SSHOPTS="-o ControlPath=/tmp/ssh-nixos-vm-%n -o Port=$SSH_PORT"
           nixos-rebuild \
-            --flake "$FLAKE" \
+            --flake "$FLAKE_NAME" \
             --fast --target-host "$THE_TARGET" --build-host "$THE_TARGET" \
             --use-remote-sudo \
             switch "$@"
