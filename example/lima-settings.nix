fdde9e8e962dce8f170e2960401cd7eee6e18aea
Alain Lehmann <alain.lehmann@gmail.com>
2024-11-14 22:15:53 +0100

    document container registry settings

--- example/lima-settings.nix
+++ example/lima-settings.nix
@@ -1,4 +1,5 @@
 {config, ...}: {
+  #lima.settings.plain = true; #disables mounts,ports,ga,etc
   lima.settings.ssh.localPort = 2222;
   lima.settings.mounts = [
     {location = "/Users/${config.lima.user.name}";}
@@ -11,4 +12,8 @@
   lima.settings.memory = "8GB";
   lima.settings.cpus = 8;
   lima.settings.disk = "60GB";
+  virtualisation.containers.registries.search = [
+    "docker.io"
+    "quay.io"
+  ];
 }
