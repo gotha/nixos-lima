4b7675e97f8fe18e98218dc8863bd9247ee2dc64
Alain Lehmann <alain.lehmann@gmail.com>
2024-11-16 13:28:53 +0100

    simplify partioning to just ext4

--- modules/disk-config.nix
+++ modules/disk-config.nix
@@ -12,40 +12,22 @@
             size = "1M";
             type = "EF02";
           };
-          esp = {
-            name = "ESP";
-            size = "500M";
+          ESP = {
             type = "EF00";
+            size = "500M";
             content = {
               type = "filesystem";
               format = "vfat";
               mountpoint = "/boot";
+              mountOptions = ["umask=0077"];
             };
           };
           root = {
-            name = "root";
             size = "100%";
-            content = {
-              type = "lvm_pv";
-              vg = "pool";
-            };
-          };
-        };
-      };
-    };
-    lvm_vg = {
-      pool = {
-        type = "lvm_vg";
-        lvs = {
-          root = {
-            size = "100%FREE";
             content = {
               type = "filesystem";
               format = "ext4";
               mountpoint = "/";
-              mountOptions = [
-                "defaults"
-              ];
             };
           };
         };
