d834703ec82b087e1664d669e933b1dee35e5a0f
Alain Lehmann <alain.lehmann@gmail.com>
2024-11-16 16:11:27 +0100

    isolate guestagent into module

--- modules/lima.nix
+++ modules/lima.nix
@@ -33,15 +33,6 @@ with lib; let
       type = types.anything;
     };
 
-    vsockPort = mkOption {
-      type = types.ints.between 2222 2222;
-      description = ''
-        The ssh port on the host system.
-        (not sure if it is configurable)
-      '';
-      default = 2222;
-    };
-
     settings = mkOption {
       default = {};
       description = ''
@@ -127,25 +118,11 @@ with lib; let
   };
 in {
   inherit options;
-  imports = [./base.nix ./lima_bootstrap.nix ./lima_mounts.nix ./lima_rosetta.nix];
+  imports = [./base.nix ./lima_bootstrap.nix ./lima_mounts.nix ./lima_rosetta.nix ./lima_guestagent.nix];
   config = {
     lima.configFile = configFile;
     lima.settings = {
       inherit images containerd;
-    };
-
-    systemd.services.lima-guestagent = {
-      enable = true;
-      description = "lima-guestagent for port forwarding";
-      wantedBy = ["multi-user.target"];
-      after = ["network.target"];
-      serviceConfig = {
-        Type = "simple";
-        # this get everything into the VM -- even qemu, not just the guestagent
-        # ExecStart = "${pkgs.lima-bin}/share/lima/lima-guestagent.Linux-aarch64 daemon";
-        ExecStart = "${cfg.cidata}/lima-guestagent daemon --vsock-port ${toString cfg.vsockPort}";
-        Restart = "on-failure";
-      };
     };
   };
 }
---
+++ modules/lima_guestagent.nix
@@ -0,0 +1,36 @@
+{
+  config,
+  lib,
+  ...
+}:
+with lib; let
+  cfg = config.lima;
+
+  options.lima = {
+    vsockPort = mkOption {
+      type = types.ints.between 2222 2222;
+      description = ''
+        The ssh port on the host system.
+        (not sure if it is configurable)
+      '';
+      default = 2222;
+    };
+  };
+in {
+  inherit options;
+  config = {
+    systemd.services.lima-guestagent = {
+      enable = true;
+      description = "lima-guestagent for port forwarding";
+      wantedBy = ["multi-user.target"];
+      after = ["network.target"];
+      serviceConfig = {
+        Type = "simple";
+        # this get everything into the VM -- even qemu, not just the guestagent
+        # ExecStart = "${pkgs.lima-bin}/share/lima/lima-guestagent.Linux-aarch64 daemon";
+        ExecStart = "${cfg.cidata}/lima-guestagent daemon --vsock-port ${toString cfg.vsockPort}";
+        Restart = "on-failure";
+      };
+    };
+  };
+}
