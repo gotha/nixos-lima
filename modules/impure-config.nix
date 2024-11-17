cf7596741ba8796ae534e7882e4cf4a9ec52de64
Alain Lehmann <alain.lehmann@gmail.com>
2024-11-17 18:20:11 +0100

    support multiple external configs

--- modules/impure-config.nix
+++ modules/impure-config.nix
@@ -1,16 +1,18 @@
 # this module allows for providing addiitonal configuration
 # via pointing NIXOS_LIMA_IMPURE_CONFIG to a nix configuration file
 # e.g. NIXOS_LIMA_IMPURE_CONFIG=$PWD/additional.nix nixos-lima mynixos start
-let
-  cfgFile = builtins.getEnv "NIXOS_LIMA_IMPURE_CONFIG";
+# the module environment variable may list multiple files (csv-like)
+{lib, ...}: let
+  cfgFilesCsv = builtins.getEnv "NIXOS_LIMA_IMPURE_CONFIG";
   pwd = builtins.getEnv "PWD";
+
+  csvToList = s:
+    lib.filter (f: lib.isString f && f != "")
+    (lib.split "," s);
   makeAbsolute = p:
-    if (builtins.substring 0 1 p) == "/"
+    if (lib.substring 0 1 p) == "/"
     then p
     else "${pwd}/${p}";
 in {
-  imports =
-    if (cfgFile != "")
-    then [(makeAbsolute cfgFile)]
-    else [];
+  imports = map makeAbsolute (csvToList cfgFilesCsv);
 }
